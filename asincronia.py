5f5ba80de54e38065dce185137ce7fd156d9a09a
arianezf <arianezf@gmail.com>
2023-03-13 16:13:24 +0000

    :sparkles: implement asynchronous task

---
+++ asincronia.py
@@ -0,0 +1,33 @@
+import asyncio
+import aiohttp
+import requests
+from datetime import datetime
+
+
+async def asynchronous_task():
+    start_time = datetime.now()
+    print("We are going to perform an asynchronous operation")
+    async with aiohttp.ClientSession() as session:
+        for i in range(500):
+            async with session.get("https://dog.ceo/api/breeds/image/random") as resp:
+                print(i ,"->",await resp.json())
+    end_time = datetime.now()
+    print('Duration async: {}'.format(end_time - start_time))
+
+
+def synchronous_task():
+    start_time = datetime.now()
+    print("We are going to perform an synchronous operation")
+    for i in range(500):
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        print(i ,"->",response.json())
+    end_time = datetime.now()
+    print('Duration sync: {}'.format(end_time - start_time))
+
+
+def main():
+    asyncio.run(asynchronous_task())
+    synchronous_task() 
+
+
+main()
